c0cddbbfb75d11b7127f47b3488ded88de3f03cd
Jerome Kaphael Ghartey <gharteyjerome@gmail.com>
2025-03-01 09:24:20 +0000

    env

--- apps/www/constants/routes.ts
+++ apps/www/constants/routes.ts
@@ -1,4 +1,8 @@
 export const routes = {
   home: "/",
   github: "https://github.com/jkghartey/aviris",
+  docs_app:
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXT_PUBLIC_DOCS_URL
+      : "http://localhost:3001",
 } as const;
